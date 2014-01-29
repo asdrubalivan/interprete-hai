2b3f7e6117be5093d9f8536aea3fb07aaf3d9afd
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2014-01-29 16:39:03 -0430

    Añadidas más reglas al dev, todavía sin fusionar a master

--- interprete/cparse.py
+++ interprete/cparse.py
@@ -14,6 +14,63 @@ def p_variable(t):
     '''
     pass
 
+def p_sign(t):
+    '''sign : PLUS 
+            | MINUS
+    '''
+    pass
+
+def p_multiplication_operator(t):
+    '''multiplication_operator : TIMES 
+                              | AND 
+                              | DIVIDE '''
+    pass
+
+def p_add_operator(t):
+    '''add_operator : PLUS
+                   | MINUS
+                   | OR
+    '''
+    pass
+
+def p_relational_operator(t):
+    '''relational_operator : LT
+                          | GT
+                          | LE
+                          | GE
+                          | EQ
+                          | NE
+    '''
+    pass
+
+def p_term(t):
+    '''term : factor
+            | multiplication_operator factor
+    '''
+    pass
+
+
+def p_factor(t):
+    '''factor : number
+              | string
+              | variable
+              | function_designator
+              | LPAREN expression RPAREN
+    '''
+    pass
+
+def p_simple_expression(t):
+    '''simple_expression : sign term
+                         | sign term add_operator term
+    '''
+    pass
+
+def p_number(t):
+    '''number : ICONST
+             | FCONST
+    '''
+    pass
+
 # Identificador de parametros
 def p_parameter_identifier(t):
     '''parameter_identifier : ID'''
@@ -21,10 +78,14 @@ def p_parameter_identifier(t):
 
 # Constante
 def p_constant(t):
-    '''constant : ICONST
-                | FCONST
-                | SCONST
+    '''constant : sign number
+                | number
+                | string
     '''
+    pass
+
+def p_string(t):
+    '''string : SCONST'''
     pass
 
 
