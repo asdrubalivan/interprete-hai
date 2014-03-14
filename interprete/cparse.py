06bab1b60c8c34da22ca0d51fbc01b5bbe1ae878
Asdrúbal Iván Suárez Rivera <asdrubal_690@yahoo.es>
2014-03-14 18:37:06 +0000

    Colocados logs en cparse.py

--- interprete/cparse.py
+++ interprete/cparse.py
@@ -1,13 +1,12 @@
-
 import sys
 import clex
 import ply.yacc as yacc
 import re
 import operator
 from settings import LOGGER_FILE, LOGGER_LEVEL
-import logging
+import logging as logger
 
-logging.basicConfig(filename=LOGGER_FILE,level=LOGGER_LEVEL)
+logger.basicConfig(filename=LOGGER_FILE,level=LOGGER_LEVEL)
 
 from utils import get_decl_total, delete_brackets, numeros_bracket
 
@@ -30,51 +29,60 @@ def p_raiz(t):
     ''' raiz : programa
     '''
     t[0] = [t[1]]
+    logger.debug("Parseando Raiz {}".format(t[0]))
 
 def p_raiz_subprogramas(t):
     ''' raiz : programa subprogramas '''
     t[0] = [t[1]] + t[2]
+    logger.debug("Parseando programas con subprogramas".format(t[¨0]))
 
 def p_raiz_empty(t):
     ''' raiz : empty '''
     t[0] = []
+    logger.debug("Raiz vacia")
 
 def p_programa(t):
     ''' programa : PROGRAMA COLON ID progvariables algoritmo FINPROGRAMA '''
     t[0] = ProgramaNodo([t[4],t[5]],t[3])
+    logger.debug("Colocando programa nodo con ID {i}, variables {v}, y algoritmo {a}".format(i=t[4],v=t[5],a=t[3]))
 
 
 def p_progvariables(t):
     ''' progvariables : VARIABLES COLON declvariables
     '''
     t[0] = t[3]
+    logger.debug("Colocando progvariables {}".format(t[0]))
 
 def p_progvariables_empty(t):
     ''' progvariables : empty '''
-    pass
+    logger.debug("Progvariables vacia")
 
 def p_subprogramas(t):
     ''' subprogramas : subprograma subprogramas
     '''
     t[0] = [t[1]] + t[2]
+    logger.debug("Colocando subprograma {s} en subprogramas (Len:{l})".format(s=t[2],l=len(t[0]))
 
 def p_subprogramas_empty(t):
     ''' subprogramas : empty '''
     t[0] = []
+    logger.debug("Subprogramas vacio")
 
 def p_subprograma(t):
     ''' subprograma : SUBPROGRAMA COLON tiporetorno ID LPAREN argssubp RPAREN progvariables algoritmosub FINSUBPROGRAMA '''
     t[0] = SubprogramaNodo([t[3],t[6],t[8],t[9]],t[4])
+    logger.debug("Subprograma nodo: {}".format(t[0]))
 
 def p_tiporetorno(t):
     ''' tiporetorno : tipo optbrackets
     '''
     t[0] = DeclaracionNodo([t[1],t[2]])
-
+    logger.debug("DeclaracionNodo (tiporetorno) {}".format(t[0]))
 
 def p_tiporetorno_empty(t):
     ''' tiporetorno : empty '''
     t[0] = VoidNodo()
+    logger.debug("Colocando VoidNodo")
 
 def p_tipo(t):
     ''' tipo : ENTERO 
@@ -82,40 +90,51 @@ def p_tipo(t):
              | CARACTER
     '''
     t[0] = t[1]
+    logger.debug("Colocando tipo {}".format(t[0]))
 
 def p_declvariables(t):
     ''' declvariables : listadecl declvariables
     '''
     t[0] = t[1]
+    logger.debug("Usando regla declvariables")
+    logger.debug("t[0] es {}".format(t[0]))
     if t[2]:
         t[0].extend(t[2])
+        logger.debug("Luego de extender t[0] es {}".format(t[0]))
+    
 
 def p_declvariables_empty(t):
     ''' declvariables : empty '''
-    pass
+    logger.debug("declvariables vacio")
 
 #TODO Anotar en las declaraciones el limite de caracteres de cada variable
 def p_listadecl(t):
     ''' listadecl : tipo listaids SEMI '''
     if t[2]:
         t[0] = [DeclaracionNodo(get_decl_total(t[1],x),delete_brackets(x),en_declvariables=True,tam_nodo=numeros_bracket(x)) for x in t[2]]
-        #print("T es : {}".format(t[0]))
+        logger.debug("Listadecl es : {}".format(t[0]))
+    else:
+        logger.warning("listadecl: listaids vacia")
 
 def p_listaids(t):
     ''' listaids : iddecl COMMA listaids
     '''
+    logger.debug("listaids: iddecl COMMA listaids, donde listaids ANTES de ser puesto como lista es {}".format(t[0]))
     t[0] = [t[1]]
+    logger.debug("Transformamos t en lista {}".format(t[0]))
     if t[3] is not None:
         if isinstance(t[3],list):
             t[0].extend(t[3])
+            logger.debug("t[3] = {t3} Es instance de list por tanto t[0] es {t0}".format(t0=t[0],t3=t[3]))
         else:
             #STRING
             t[0].append(t[3])
+            logger.debug("t[3] = {t3} NO es instance de list por tanto t[0] es {t0}".format(t0=t[0],t3=t[0]))
 
 def p_listaids_id(t):
     ''' listaids : iddecl '''
     t[0] = t[1]
-    logging.debug("Lista ids {}".format(t[0]))
+    logger.debug("Listaids : iddecl {}".format(t[0]))
 
 def p_iddecl(t):
     ''' iddecl : ID bracketsdecl
@@ -124,6 +143,7 @@ def p_iddecl(t):
     if t[2] is not None:
         temp += t[2]
     t[0] = temp
+    logger.debug("iddecl : ID bracketsdecl t[0] = {t0}".format(t0=t[0]))
 
 
 
@@ -137,6 +157,7 @@ def p_idvariable(t):
     if t[2] is not None:
         temp += t[2]
     t[0] = temp
+    logger.debug("idvariable : ID bracketsvar t[0] = {t0}".format(t0=t[0]))
 
 def p_bracketsvar(t):
     ''' bracketsvar : LBRACKET ICONST RBRACKET bracketsvar
@@ -147,6 +168,9 @@ def p_bracketsvar(t):
         t[0] = t[1] + str(t[2]) + t[3]
         if t[4] is not None:
             t[0] += t[4]
+        logger.debug("bracketsvar = {t0}".format(t0=t[0]))
+    else:
+        logger.debug("bracketsvar vacio")
 
 
 def p_bracketsdecl(t):
@@ -157,16 +181,19 @@ def p_bracketsdecl(t):
         t[0] = t[1] + str(t[2]) + t[3]
         if t[4] is not None:
             t[0] += t[4]
+        logger.debug("bracketsdecl = {t0}".format(t0=t[0]))
 def p_algoritmo(t):
     ''' algoritmo : ALGORITMO COLON listasentencias FINALGORITMO
     '''
     t[0] = AlgoritmoNodo([t[3]])
+    logger.debug("Colocando algoritmo nodo {t0}".format(t0=t[0]))
 
 #Algoritmo en Subprograma
 
 def p_algoritmosub(t):
     ''' algoritmosub : ALGORITMO COLON listasentencias FINALGORITMO '''
     t[0] = AlgoritmoSubNodo([t[3],t[4]])
+    logger.debug("Algoritmo en subprograma {t0}".format(t0=t[0]))
 
 def p_listasentencias(t):
     ''' listasentencias : sentencia SEMI listasentencias
@@ -174,16 +201,18 @@ def p_listasentencias(t):
     t[0] = [t[1]]
     if t[3] is not None:
         t[0] += t[3]
+    logger.debug("Lista de sentencias {t0}".format(t0=t[0]))
 
 def p_listasentencias_sencomp(t):
     ''' listasentencias : sencomp listasentencias '''
     t[0] = [t[1]]
     if t[2] is not None:
         t[0] += t[2]
+    logger.debug("Lista de sentencias {t0}".format(t0=t[0]))
 
 def p_listasentencias_empty(t):
     ''' listasentencias : empty '''
-    pass
+    logger.debug("Lista de sentencias empty {t0}".format(t0=t[0]))
 
 def p_sentencia(t):
     ''' sentencia : asignacion
@@ -191,18 +220,22 @@ def p_sentencia(t):
                   | senescribir
     '''
     t[0] = t[1]
+    logger.debug("Sentencia (asignacion, senleer, senescribir): {t0}".format(t0=t[0]))
 
 def p_sentencia_expr(t):
     ''' sentencia : expresion '''
     t[0] = DummyNodo()
+    logger.debug("Sentencia dummy")
 
 def p_senleer(t):
     ''' senleer : LEER idvariable '''
     t[0] = LeerNodo([t[2]],t[1])
+    logger.debug("Sentencia leer, con variable {t0}".format(t0=t[0])
 
 def p_senescribir(t):
     ''' senescribir : ESCRIBIR expresion '''
     t[0] = EscribirNodo([t[2]],t[1])
+    logger.debug("Sentencia escribir, con variable {t0}".format(t0=t[0])
 
 def p_sencomp(t):
     ''' sencomp : bloquesi
@@ -211,21 +244,25 @@ def p_sencomp(t):
                 | bloquehm
     '''
     t[0] = t[1]
+    logger.debug("Sentencia compleja, con variable {t0}".format(t0=t[0])
 
 def p_bloquesi(t):
     ''' bloquesi : SI expresion ENTONCES listasentencias FINSI
     '''
     t[0] = BloqueSiNodo([t[2],t[4],[]])
+    logger.debug("Bloque Si, con variable {t0}".format(t0=t[0])
 
 def p_bloquesi_contrario(t):
     ''' bloquesi : SI expresion ENTONCES listasentencias CONTRARIO listasentencias FINSI
     '''
     t[0] = BloqueSiNodo([t[2],t[4],t[6]])
+    logger.debug("Sentencia leer, con variable {t0}".format(t0=t[0])
 
 def p_asignsimple(t):
     ''' asignsimple : idvariable EQUALS expresion
     '''
     t[0] = AsigNodo([t[1],t[3]],t[2])
+    logger.debug("Asignacion simple, con variable {t0}".format(t0=t[0])
 
 # Asignacion en Repita para
 # NOTE Se usa simple debido a que no
@@ -235,31 +272,37 @@ def p_asigrp(t):
                | idvariable
     '''
     t[0] = t[1]
+    logger.debug("Asignacion, con variable {t0}".format(t0=t[0])
 
 def p_bloquerp(t):
     ''' bloquerp : REPITAPARA asigrp COMMA expresion COMMA expresion COLON listasentencias FINRP
     '''
     t[0] = BloqueRpNodo([t[2],t[4],t[6],t[8]])
+    logger.debug("Bloque Repita para, con variable {t0}".format(t0=t[0])
 
 def p_bloquerm(t):
     ''' bloquerm : REPITAMIENTRAS expresion COLON listasentencias FINRM
     '''
     t[0] = BloqueRmNodo([t[2],t[4]])
+    logger.debug("Bloque repita mientras, con variable {t0}".format(t0=t[0])
 
 def p_bloquehm(t):
     ''' bloquehm : HAGA COLON listasentencias MIENTRAS LPAREN expresion RPAREN 
     '''
     t[0] = BloqueHmNodo([t[6],t[3]])
+    logger.debug("Bloque haga mientras, con variable {t0}".format(t0=t[0])
 
 def p_expresion(t):
     ''' expresion : NOT expresion
     '''
     t[0] = NegacionNodo([t[2]])
+    logger.debug("Expresion negacion, con variable {t0}".format(t0=t[0])
 
 def p_expression_paren(t):
     ''' expresion : LPAREN expresion RPAREN
     '''
     t[0] = t[2]
+    logger.debug("Expresion parentesis con variable {t0}".format(t0=t[0]))
 
 def p_expression_unvalor(t):
     ''' expresion : literal
@@ -268,6 +311,7 @@ def p_expression_unvalor(t):
                   | operacionbin
     '''
     t[0] = t[1]
+    logger.debug("Expresion con variable {t0}".format(t0=t[0]))
 
 def p_literal(t):
     ''' literal : ICONST
@@ -275,29 +319,35 @@ def p_literal(t):
                 | SCONST
     '''
     t[0] = LiteralNodo(hoja=t[1])
+    logger.debug("Literal con variable {t0}".format(t0=t[0]))
 
 def p_llamadafunc(t):
     ''' llamadafunc : ID LPAREN arglista RPAREN '''
     t[0] = LlamadaFuncNodo(t[3],t[1])
+    logger.debug("Llama de funcion con variable {t0}".format(t0=t[0]))
 
 def p_arglista(t):
     ''' arglista : expresion
     '''
     t[0] = [t[1]]
+    logger.debug("Lista de argumenos variable {t0}".format(t0=t[0]))
 
 def p_arglista_listas(t):
     ''' arglista : arglista COMMA expresion '''
     t[0] = [t[1]]
     if t[3] is not None:
         t[0] += t[3]
+        logger.debug("Colocando nueva expresion en arglista {t3}".format(t3=t[3]))
+    logger.debug("Lista de argumentos con variable {t0}".format(t0=t[0]))
 
 def p_arglista_empty(t):
     ''' arglista : empty '''
-    pass
+    logger.debug("arglista : empty")
 
 def p_operacionbin(t):
     ''' operacionbin : expresion operador expresion '''
     t[0] = BinOpNodo([t[1],t[3]],t[2])
+    logger.debug("Operacion binaria {izq} {op} {der}".format(izq=t[1],der=t[3],op=t[2]))
 
 def p_operador(t):
     ''' operador : PLUS
@@ -319,6 +369,7 @@ def p_operador(t):
 def p_asignacion(t):
     ''' asignacion : idvariable asignador expresion '''
     t[0] = AsigNodo([t[1],t[3]],t[2])
+    logger.debug("Asignacion con variable {t0}".format(t0=t[0]))
 
 def p_asignador(t):
     ''' asignador : EQUALS
@@ -331,6 +382,7 @@ def p_argssubp(t):
     ''' argssubp : paramsub
     '''
     t[0] = t[1]
+    logger.debug("Argssub : paramsub con variable {t0}".format(t0=t[0]))
 
 def p_argssubp_comma(t):
     ''' argssubp : paramsub COMMA argssubp
@@ -338,16 +390,18 @@ def p_argssubp_comma(t):
     t[0] = [t[1]]
     if t[3] is not None:
         t[0] += t[3]
+        logger.debug("Colocando argsub {}".format(t[3]))
+    logger.debug("Expresion argsub : paramsub COMMA argssubp con variable {t0}".format(t0=t[0]))
 
 def p_argssubp_empty(t):
     ''' argssubp : empty '''
-    pass
+    logger.debug("Argssubp vacio")
 
 def p_paramsub(t):
     ''' paramsub : tiporetorno ID '''
     t[1].hoja = t[2] #Anadimos la ID
     t[0] = [t[1]]
-    #print(t[0])
+    logger.debug("paramsub : tiporetorno con variable {t0}".format(t0=t[0]))
 
 def p_optbrackets(t):
     ''' optbrackets : LBRACKET RBRACKET optbrackets
@@ -365,10 +419,13 @@ def p_retorno(t):
     ''' retorno : RETORNE idvariable SEMI
     '''
     t[0] = RetornoNodo([t[2]])
+    logger.debug("Retorno nodo con variable {t0}".format(t0=t[0]))
 
+#TODO Quitar esto
 def p_retorno_empty(t):
     ''' retorno : empty '''
     t[0] = RetornoNodo()
+    logger.debug("Retorno nodo vacio con variable")
 
 #Vacio
 def p_empty(t):
